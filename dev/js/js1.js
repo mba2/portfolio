62aa12b2c8b23d1daaab5aa64cbb229bd394f7b7
Mario - Tests <marinhomala@hotmail.com>
2017-01-15 14:28:44 -0200

    COMMIT TEST - MADE BY TESTER USER

--- dev/js/js1.js
+++ dev/js/js1.js
@@ -2,3 +2,4 @@ console.log("Made in Windows - Tester User");
 console.log("Made on master - 01");
 console.log("Made on master - 02");
 console.log("master branch");
+console.log("Made in Linux - Master Branch - More things here --- !!!");
