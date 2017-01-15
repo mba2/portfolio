78bffb35e846658a484c796d03a365a6f68d2d00
Mario Brusarosco <mariobrusarosco@gmail.com>
2017-01-15 15:10:42 -0200

    TESTING BRANCH REBASE - MADE IN 'master' (2)

--- dev/js/js1.js
+++ dev/js/js1.js
@@ -4,3 +4,4 @@ console.log("Made on master - 02");
 console.log("master branch");
 console.log("Made in Linux - Master Branch - More things here --- !!!");
 console.log("Made in Linux - Master Branch --- !!!");
+console.log("Made in Linux - Master Branch - More things here --- !!!");
