9843a88d383eb9af437d5534a73304918494f16d
Mario Brusarosco <mariobrusarosco@gmail.com>
2017-01-22 20:28:16 -0200

    js 'mater' branch

--- dev/js/js1.js
+++ dev/js/js1.js
@@ -1,4 +1,10 @@
+<<<<<<< 2427b7bf50129dbb05e71527169f411c283c79ab
 console.log("1");
 console.log("Made in Windows - Tester User");
 console.log("Made on master - 01");
 console.log("Made on master - 02");
+=======
+console.log("made on test branch");
+
+console.log("made on master branch");
+>>>>>>> js 'mater' branch
