9485130fd733cda9b0640cadb0cd6d7b6288599c
Mario Brusarosco <mariobrusarosco@gmail.com>
2017-01-22 20:32:16 -0200

    js removed breaklines 'tests' branch

--- dev/js/js1.js
+++ dev/js/js1.js
@@ -2,3 +2,7 @@ console.log("1");
 console.log("Made in Windows - Tester User");
 console.log("Made on master - 01");
 console.log("Made on master - 02");
+console.log("made on test branch");
+console.log("made on master branch");
+console.log("made on master branch");
+console.log("inserted on tests branch");
