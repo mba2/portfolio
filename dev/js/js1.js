4e5d06175a9ec6bd1e5d541da800b01dcccc9aac
Mario Brusarosco <mariobrusarosco@gmail.com>
2017-01-22 20:26:47 -0200

    js 'tests' branch

--- dev/js/js1.js
+++ dev/js/js1.js
@@ -1,11 +1,1 @@
-console.log("Made in Windows - Tester User");
-console.log("Made on master - 01");
-console.log("Made on master - 02");
-console.log("master branch");
-console.log("Made in Linux - Master Branch - More things here --- !!!");
-console.log("Made in Linux - Master Branch --- !!!");
-console.log("Made in Linux - Master Branch - More things here --- !!!");
-console.log("master branch");
-console.log("Made in Linux - Master Branch - More things here --- !!!");
-console.log("Made in Linux - Master Branch --- !!!");
-console.log("Made in Linux - Master Branch - More things here --- !!!");
+console.log("made on test branch");
