2210bd16b21d3c896aeb8013964645f79281f162
Mario Brusarosco <mariobrusarosco@gmail.com>
2017-01-22 20:31:29 -0200

    js added a new line  'tests' branch

--- dev/js/js1.js
+++ dev/js/js1.js
@@ -1,11 +1,10 @@
 console.log("made on test branch");
 
 
+console.log("made on master branch");
 
 
 console.log("made on master branch");
 
 
-
-
-console.log("made on master branch");
+console.log("inserted on tests branch");
