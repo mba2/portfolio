a845414760a563ce4df6e9397696e84fa1c4504b
Mario Brusarosco <mariobrusarosco@gmail.com>
2017-01-22 20:36:28 -0200

    js altert 'test' branch

--- dev/js/js1.js
+++ dev/js/js1.js
@@ -4,12 +4,3 @@ console.log("Made on master - 01");
 console.log("Made on master - 02");
 console.log("made on test branch");
 console.log("made on master branch");
-
-alert("tests");
-alert("adssadas");
-
-
-
-
-console.log("altered on tests branch");
-console.log("inserted on tests branch");
