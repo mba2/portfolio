dde415e325c6e4b86613101a6787785eed0d9c55
Mario Brusarosco <mariobrusarosco@gmail.com>
2017-01-22 20:49:12 -0200

    tests

--- dev/js/js1.js
+++ dev/js/js1.js
@@ -1,1 +1,12 @@
+<<<<<<< eca18c7d5a3ab311b58409f8bdcd204e117bb544
 console.log("made on test branch");
+=======
+console.log("made  on master branch");
+
+
+
+
+
+
+console.log("altered on test branch");
+>>>>>>> tests
