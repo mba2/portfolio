99c85c104582dce32543281139ad4a6354b265e4
Mario Brusarosco <mariobrusarosco@gmail.com>
2017-01-22 20:49:12 -0200

    tests

--- dev/js/js1.js
+++ dev/js/js1.js
@@ -1,5 +1,19 @@
+<<<<<<< d82fa0c8711ea797e6335284663b4b11f15bfc5f
 console.log("1");
 console.log("Made in Windows - Tester User");
 console.log("Made on master - 01");
 console.log("Made on master - 02");
+=======
+<<<<<<< eca18c7d5a3ab311b58409f8bdcd204e117bb544
+>>>>>>> tests
 console.log("made on test branch");
+=======
+console.log("made  on master branch");
+
+
+
+
+
+
+console.log("altered on test branch");
+>>>>>>> tests
