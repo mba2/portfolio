6cee173334319209a938ad76dfa1fcd899fa3fbc
Mario Brusarosco <mariobrusarosco@gmail.com>
2017-01-29 16:58:58 -0200

    conflict

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,24 +48,6 @@ module.exports = function(grunt){
         }
       }
     },
-<<<<<<< HEAD
-=======
-    //
-    // "sass_compile_imports" : {
-    //   "compile" : {
-    //     options : {
-    //       quiet : false
-    //     },
-    //     "target" : "<%= dirs.development %>/smacss/_import.scss",
-    //     files : [{
-    //       expand : true,
-    //       cwd : "<%= dirs.development %>/smacss/",
-    //       src : ["!_import.scss", "btn.scss"]
-    //     }]
-    //   }
-    // },
-
->>>>>>> header
     sass_import: {
        options: {},
        dist: {
@@ -228,11 +210,7 @@ module.exports = function(grunt){
 
   var mainTasks = [
     "clean:build",
-<<<<<<< HEAD
-    "sprite",
-=======
     // "sprite",
->>>>>>> header
     "sass_import",
     "compass",
     "cssmin:" + ( assetsMode || "single_file" ),
