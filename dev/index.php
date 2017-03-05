f799b60381aba99ce978e63138e8caf3143d4892
Mario - Tests <marinhomala@hotmail.com>
2017-03-05 19:42:23 -0300

    CSS: FOOTER. HOVER EFFECTS ON SVG ICONS

--- dev/index.php
+++ dev/index.php
@@ -14,7 +14,7 @@
 
   <div class="l-wrapper-main">
     <header id="js_header" class="l-header">
-        <button id="js_headerMenuBtn" class="btn btn-menu mdElem">
+        <button id="js_headerMenuBtn" class="btn btn-menu mobileElem">
           <span class="hambLine hambLine-top"></span>
           <span class="hambLine hambLine-center"></span>
           <span class="hambLine hambLine-bottom"></span>
@@ -28,7 +28,7 @@
 
         <nav id="js_headerNav" class="nav nav-header">
           <menu id="js_headerMenu" class="menu menu-header">
-            <ul id="js_menuList" class="menulist">
+            <ul id="js_menuList" class="menu-list">
               <li class="menu-listItem"><a id="" class="menu-link" href="/resumee">resume</a></li>
               <li class="menu-listItem"><a id="" class="menu-link" href="/snippets">snippets</a></li>
               <li class="menu-listItem"><a id="" class="menu-link" href="/desk-apps">desktop apps</a></li>
@@ -57,18 +57,44 @@
     <footer id="js_footer" class="l-footer">
       <div id="js_footerWrapper" class="wrapper">
         <ul id="js_footerMenu" class="menu menu-footer">
-          <li id="js_footerEmail" class="link link-menu link-email">
+          <li id="js_footerEmail" class="link link-menu link-footer link-email">
             <a href="#">mariobrusarosco@gmail.com</a>
           </li>
           <div id="" class="link-socials">
-            <li id="js_footerTwitter" class="link link-menu link-twitter">
+            <li id="js_footerTwitter" class="link link-menu link-footer link-twitter">
               <a href="https://twitter.com/mariobrusarosco">twitter
-                <img class="icon" src="img/twitter-icon.svg" alt="">
+                <svg class="icon" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                	 viewBox="0 0 612 612" style="enable-background:new 0 0 612 612;" xml:space="preserve">
+                  <g>
+                  	<g>
+                  		<path d="M612,116.258c-22.525,9.981-46.694,16.75-72.088,19.772c25.929-15.527,45.777-40.155,55.184-69.411
+                  			c-24.322,14.379-51.169,24.82-79.775,30.48c-22.907-24.437-55.49-39.658-91.63-39.658c-69.334,0-125.551,56.217-125.551,125.513
+                  			c0,9.828,1.109,19.427,3.251,28.606C197.065,206.32,104.556,156.337,42.641,80.386c-10.823,18.51-16.98,40.078-16.98,63.101
+                  			c0,43.559,22.181,81.993,55.835,104.479c-20.575-0.688-39.926-6.348-56.867-15.756v1.568c0,60.806,43.291,111.554,100.693,123.104
+                  			c-10.517,2.83-21.607,4.398-33.08,4.398c-8.107,0-15.947-0.803-23.634-2.333c15.985,49.907,62.336,86.199,117.253,87.194
+                  			c-42.947,33.654-97.099,53.655-155.916,53.655c-10.134,0-20.116-0.612-29.944-1.721c55.567,35.681,121.536,56.485,192.438,56.485
+                  			c230.948,0,357.188-191.291,357.188-357.188l-0.421-16.253C573.872,163.526,595.211,141.422,612,116.258z"/>
+                  	</g>
+                  </g>
+                </svg>
               </a>
             </li>
-            <li id="js_footerLinked" class="link link-menu link-linkedin">
+            <li id="js_footerLinked" class="link link-menu link-footer link-linkedin">
               <a href="https://www.linkedin.com/in/mariobrusarosco/">linkedin
-                <img class="icon" src="img/linkedin-icon.svg" alt="">
+                <svg class="icon" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                	 width="430.117px" height="430.118px" viewBox="0 0 430.117 430.118" style="enable-background:new 0 0 430.117 430.118;"
+                	 xml:space="preserve">
+                  <g>
+                  	<path id="LinkedIn__x28_alt_x29_" d="M398.355,0H31.782C14.229,0,0.002,13.793,0.002,30.817v368.471
+                  		c0,17.025,14.232,30.83,31.78,30.83h366.573c17.549,0,31.76-13.814,31.76-30.83V30.817C430.115,13.798,415.904,0,398.355,0z
+                  		 M130.4,360.038H65.413V165.845H130.4V360.038z M97.913,139.315h-0.437c-21.793,0-35.92-14.904-35.92-33.563
+                  		c0-19.035,14.542-33.535,36.767-33.535c22.227,0,35.899,14.496,36.331,33.535C134.654,124.415,120.555,139.315,97.913,139.315z
+                  		 M364.659,360.038h-64.966V256.138c0-26.107-9.413-43.921-32.907-43.921c-17.973,0-28.642,12.018-33.327,23.621
+                  		c-1.736,4.144-2.166,9.94-2.166,15.728v108.468h-64.954c0,0,0.85-175.979,0-194.192h64.964v27.531
+                  		c8.624-13.229,24.035-32.1,58.534-32.1c42.76,0,74.822,27.739,74.822,87.414V360.038z M230.883,193.99
+                  		c0.111-0.182,0.266-0.401,0.42-0.614v0.614H230.883z"/>
+                  </g>
+                </svg>
               </a>
             </li>
           </div>
