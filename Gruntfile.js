84d56d5554fa3cbddd4ced0ab0032e54dbfa9824
Mario Brusarosco <mariobrusarosco@gmail.com>
2017-04-22 11:48:35 -0300

    CLEAN TASK RUNS AND CLEANS BASED ON THE GIVEN TASK NAME

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function( grunt ){
 
   grunt.initConfig({
+    // FOLDERS PATHS, STORED IN VARIABLES
     dir : {
       "src"     : "src",
       "dev"     : "dev",
@@ -9,18 +10,30 @@ module.exports = function( grunt ){
       "js"      : "js",
       "fonts"   : "fonts",
       "images"  : "img",
-      "currTask": grunt.cli.tasks[0]
+      "currTask": grunt.cli.tasks[0] || "build"
     },
-    //CLEAN
+    //CLEAN TASK
     clean : {
       target : {
-        src : "<%= dir.currTask %>"
+        src : "<%= dir.currTask %>/**/*"
+      }
+    },
+    //RESPONSIVE IMAGES TASK
+    responsive_images : {
+      target : {
+        options : {
+          sizes : [
+            
+          ]
+        },
+
       }
     }
   });
   // LOAD TASKS
-  grunt.loadNpmTasks("grunt-contrib-copy");
   grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-responsive-images");
 
 
   grunt.registerTask("default",[
