a14ff2dbb1af1bfc6f27aa89c55e7724f0fb6162
Mario Brusarosco <mariobrusarosco@gmail.com>
2017-06-18 12:43:09 -0300

    HOME - MOBILE - RESPONSIVE UNTIL 320px

--- dev/js/w_Page.js
+++ dev/js/w_Page.js
@@ -21,14 +21,19 @@ var Page = {
     "backToTopBtn" : document.querySelector("#js_back-to-top")  // DOM SELECTION OF THE BACK TO TOP BTN
   },
   goToTop : function() {
-    var _this = this,
-                                            // Reference to Page Object
+    var _this = this, // Reference to Page Object
+
     setClickAction = function(){
       // CLICK EVENT
       _this.DOM.backToTopBtn.addEventListener("click", function(e){
+
         $("html,body").animate({
-          scrollTop : 0
-        },1000);}, false);
+            scrollTop : 0
+         },1000, function() {
+                  document.querySelector("body").classList.remove("menu-is-opened");
+               }
+         );
+     });
     }();
     btnVisibility = function(){
          var startPoint = 77,            // FROM THIS POINT, THE BACK TO TOP BTN MUST BE VISIBLE
@@ -36,17 +41,16 @@ var Page = {
 
       $(window).scroll(function(){                   // IN EACH SCROLL ON THE PAGE
         if(_this.getScrollTop() > startPoint && !btnIsVisibile) {     // IF THE AMOUT OF SCROLL IS GREATER THAN THE START POINT LIMIT  (startPoint VARIABLE)
-          console.log("Show the button!");
           _this.DOM.backToTopBtn.classList.toggle("is-visible");
           btnIsVisibile = true;
           // return true;
         }else if( _this.getScrollTop() < startPoint && btnIsVisibile ){
-          console.log("Hide the button!");
           _this.DOM.backToTopBtn.classList.toggle("is-visible");
           btnIsVisibile = false;
         }
       });
     }();
+
   },
   initOnLoad : function(){
     this.goToTop();
